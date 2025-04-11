e7f4ef9ffe0d71a3ce1f8e87100ec48e3b609ad7
Deepak Sheoran <sheorandeepak1999@gmail.com>
2025-04-11 21:19:41 +0530

    Create Section Query Statements and their usage.sql

---
+++ Course - The Ultimate MySQL Bootcamp Go From SQL Beginner to Expert/Section 5 - CRUD Basics/Section Query Statements and their usage.sql
@@ -0,0 +1,16 @@
+USE pet_shop;
+SHOW TABLES;
+
+-- SELECT
+SELECT * FROM cats;
+SELECT Name FROM cats;
+SELECT Age FROM cats;
+SELECT Name, Age FROM cats;
+
+-- WHERE
+SELECT *
+FROM cats
+WHERE Age = 4;
+SELECT NAME FROM cats WHERE Age = 4;
+
+-- 
