bbd8e1da1371f5dce1a1bca81dd48eb5acb1a5b6
Deepak Sheoran <sheorandeepak1999@gmail.com>
2025-04-16 15:52:42 +0530

    Update String Functions.sql

--- Course - The Ultimate MySQL Bootcamp Go From SQL Beginner to Expert/Section 7 - String Functions/String Functions.sql
+++ Course - The Ultimate MySQL Bootcamp Go From SQL Beginner to Expert/Section 7 - String Functions/String Functions.sql
@@ -13,3 +13,51 @@ FROM Books;
 -- 2. CONCAT_WS
 SELECT Book_id, CONCAT_WS(' ', Author_fname, Author_lname) AS 'Author Name'
 FROM Books;
+
+-- 3. SUBSTRING
+SELECT CONCAT(Author_fname, ' ', SUBSTRING(Author_lname FROM 1 FOR 1), '.') AS 'Author Name' -- An example of combining string function
+FROM Books;
+
+-- Combining String Functions
+SELECT CONCAT(SUBSTR(Title FROM 1 FOR 10), '...') AS 'Shortened Title'
+FROM Books;
+
+SELECT CONCAT(SUBSTR(Author_fname, 1, 1), '.', SUBSTRING(Author_lname, 1, 1), '.') AS 'Author\'s Initials'
+FROM Books;
+
+
+-- 4. REPLACE
+SELECT REPLACE(Title, ' ', '-') AS 'Hyphen in Place of Whitespace'
+FROM Books;
+
+-- 5. REVERSE
+SELECT CONCAT(Author_fname, REVERSE(Author_fname)) AS Palindrome
+FROM Books;
+
+-- 6. CHAR_LENGTH and LENGTH
+SELECT Title, CONCAT(CHAR_LENGTH(Title), ' characters') AS Length, CONCAT(LENGTH(Title), ' Bytes') AS 'Size in Bytes'
+FROM Books;
+
+-- 7. UPPER and LOWER
+SELECT Title, UCASE(Title) AS 'Upper Case Title', LCASE(Title) AS 'Lower Case Title'
+FROM Books;
+
+SELECT CONCAT('I LOVE ', UPPER(Title), ' !!!') AS 'Announcement'
+FROM Books;
+
+-- 8. INSERT
+SELECT INSERT('Hello Bobby', 6, 0, 'There');
+
+-- 9. INSTR
+SELECT INSTR('Hello World', 'Wor');
+
+-- 10. LEFT and RIGHT
+SELECT LEFT('omghahalol!', 3);
+SELECT RIGHT('omghahalol!', 4);
+
+-- 11. REPEAT
+SELECT REPEAT('Ha', 4);
+
+-- 12. TRIM
+SELECT TRIM('  San Antonio   '), TRIM(LEADING 'x' FROM 'xxxBarxxx');
+
