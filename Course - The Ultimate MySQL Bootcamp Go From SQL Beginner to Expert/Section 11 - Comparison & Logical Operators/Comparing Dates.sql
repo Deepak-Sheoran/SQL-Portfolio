73f41c09b21aef743f6dc6dc2dc2e9f0b1785078
Deepak Sheoran <sheorandeepak1999@gmail.com>
2025-06-21 18:12:41 +0530

    Update Comparing Dates.sql

--- Course - The Ultimate MySQL Bootcamp Go From SQL Beginner to Expert/Section 11 - Comparison & Logical Operators/Comparing Dates.sql
+++ Course - The Ultimate MySQL Bootcamp Go From SQL Beginner to Expert/Section 11 - Comparison & Logical Operators/Comparing Dates.sql
@@ -10,3 +10,16 @@ FROM people;
 SELECT *
 FROM people
 WHERE YEAR(BirthDate) < 2005;
+
+-- People born after noon
+SELECT *
+FROM people
+WHERE Hour(BirthTime) > 12;
+
+-- Using Cast
+SELECT CAST('2009-09-11 09:53:27' AS DATE) AS 'Date';
+
+SELECT *
+FROM people
+WHERE BirthTime BETWEEN CAST('10:00:00' AS TIME) AND CAST('14:00:00' AS TIME);
+
