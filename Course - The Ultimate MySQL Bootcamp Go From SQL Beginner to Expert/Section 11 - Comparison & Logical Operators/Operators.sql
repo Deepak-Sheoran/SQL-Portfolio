46a5be7823db0a8dc334d369200346a02dd0de44
Deepak Sheoran <sheorandeepak1999@gmail.com>
2025-06-23 19:04:30 +0530

    Updates

--- Course - The Ultimate MySQL Bootcamp Go From SQL Beginner to Expert/Section 11 - Comparison & Logical Operators/Operators.sql
+++ Course - The Ultimate MySQL Bootcamp Go From SQL Beginner to Expert/Section 11 - Comparison & Logical Operators/Operators.sql
@@ -1,5 +1,6 @@
 SHOW DATABASES;
 USE book_store;
+SELECT DATABASE();
 SHOW TABLES;
 DESC books;
 
@@ -52,4 +53,41 @@ FROM books
 WHERE Released_year BETWEEN 2004 AND 2015;
 
 -- 7. IN
+SELECT Title, Author_lname
+FROM books
+WHERE Author_lname IN ('Carver', 'Lahiri', 'Smith');
 
+-- 8. NOT IN
+SELECT Title, Released_year
+FROM books
+WHERE 
+	Released_year BETWEEN 2000 AND 2016
+    AND
+    Released_year NOT IN (2000, 2002, 2004, 2006, 2008, 2010, 2012, 2014, 2016)
+ORDER BY Released_year;
+
+-- OR
+
+SELECT Title, Released_year
+FROM books
+WHERE Released_year % 2 <> 0
+ORDER BY Released_year;
+
+-- CASE statements
+SELECT Title, Released_year,
+	CASE
+		WHEN Released_year >= 2000 THEN 'Modern Lit'
+        ELSE '20th Century Lit'
+    END AS Genre
+FROM books;
+
+SELECT Title, Stock_quantity,
+CASE
+	WHEN Stock_quantity BETWEEN 0 AND 40 THEN '*'
+    WHEN Stock_quantity BETWEEN 41 AND 70 THEN '**'
+    WHEN Stock_quantity BETWEEN 71 AND 100 THEN '***'
+    WHEN Stock_quantity BETWEEN 101 AND 140 THEN '****'
+    ELSE '*****'
+END AS Stock
+FROM books
+ORDER BY Stock_quantity;
