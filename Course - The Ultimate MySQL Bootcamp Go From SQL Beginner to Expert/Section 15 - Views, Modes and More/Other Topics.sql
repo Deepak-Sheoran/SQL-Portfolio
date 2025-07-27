d5de642c638413a3a361062c07e107930e6dbab6
Deepak Sheoran <sheorandeepak1999@gmail.com>
2025-07-28 01:57:58 +0530

    Update Other Topics.sql

--- Course - The Ultimate MySQL Bootcamp Go From SQL Beginner to Expert/Section 15 - Views, Modes and More/Other Topics.sql
+++ Course - The Ultimate MySQL Bootcamp Go From SQL Beginner to Expert/Section 15 - Views, Modes and More/Other Topics.sql
@@ -10,8 +10,9 @@ SELECT *
 FROM Full_Reviews;
 
 -- HAVING clause
-SELECT Title, AVG(Rating) AS 'Average Rating', COUNT(Title) AS 'Review Count'
+SELECT Title, ROUND(AVG(Rating), 2) AS 'Average Rating', COUNT(Title) AS 'Review Count'
 FROM Full_Reviews
 GROUP BY Title
 HAVING COUNT(Title) >= 3
 ORDER BY COUNT(Title);
+
